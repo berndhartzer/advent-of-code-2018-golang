d1c34a0657456aaef014660cbe194f42fd8f5046
Bernd Hartzer <berndhartzer@gmail.com>
2019-11-27 20:18:10 +1000

    day three part one

---
+++ 3_no_matter_how_you_slice_it/3_no_matter_how_you_slice_it.go
@@ -0,0 +1,51 @@
+package three_no_matter_how_you_slice_it
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+var fabric map[string]int
+
+type Claim struct {
+	ID, X, Y, W, H int
+}
+
+func NoMatterHowYouSliceItPartOne(claims []string) int {
+	fabric = make(map[string]int)
+	getNumsRegex := regexp.MustCompile("[0-9]+")
+
+	for _, raw := range claims {
+		var nums = []int{}
+
+		rawNums := getNumsRegex.FindAllString(raw, -1)
+		for _, rawNum := range rawNums {
+			num, _ := strconv.Atoi(rawNum)
+			nums = append(nums, num)
+		}
+
+		claim := Claim{
+			nums[0],
+			nums[1],
+			nums[2],
+			nums[3],
+			nums[4],
+		}
+
+		for i := claim.X; i < claim.X + claim.W; i++ {
+			for j := claim.Y; j < claim.Y + claim.H; j++ {
+				fabric[fmt.Sprintf("%d,%d", i, j)] += 1
+			}
+		}
+	}
+
+	overlaps := 0
+	for _, value := range fabric {
+		if value > 1 {
+			overlaps += 1
+		}
+	}
+
+	return overlaps
+}
---
+++ 3_no_matter_how_you_slice_it/3_no_matter_how_you_slice_it_test.go
@@ -0,0 +1,39 @@
+package three_no_matter_how_you_slice_it
+
+import (
+	"testing"
+	"os"
+	"log"
+	"bufio"
+)
+
+func TestNoMatterHowYouSliceItPartOneExampleInput(t *testing.T) {
+	input := []string{
+		"#1 @ 1,3: 4x4",
+		"#2 @ 3,1: 4x4",
+		"#3 @ 5,5: 2x2",
+	}
+	expected := 4
+	output := NoMatterHowYouSliceItPartOne(input)
+
+	if output != expected {
+		t.Fail()
+	}
+}
+
+func TestNoMatterHowYouSliceItPartOneActualInput(t *testing.T) {
+	file, err := os.Open("./3_input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var input []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
+	}
+
+	output := NoMatterHowYouSliceItPartOne(input)
+	t.Log(output)
+}
